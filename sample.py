846cbbb65a961e59c3cf928416c2a634edcd4883
Rhoda <rhodachela@gmail.com>
2024-09-27 15:57:38 +0300

    Change from the new-branch

--- sample.py
+++ sample.py
@@ -1,1 +1,7 @@
-print("This is the sample for the merge conflict")
+name = input("Enter name: ").upper()
+if name == "Chela":
+    print("Hey Chela!")
+elif name == "Bob":
+    print("Hey Bob, missed you.")
+else:
+    print("Who exactly are you?")
